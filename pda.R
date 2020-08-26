4e96f786b17c3000c5e012352fed45212372ad08
Wei Wei <wei.wei@yale.edu>
2020-08-26 09:53:27 -0400

    Update pda.R

--- pda.R
+++ pda.R
@@ -4,13 +4,13 @@
 # p: success probabilities (vector)
 # priora, priorb: priors for beta(alpha, beta)
 
-pda <- function(N, p, early.stop=FALSE, 
+pda <- function(N, p, nmin, early.stop=FALSE, 
                 priora=NULL, priorb=NULL) {
   #  number of treatment arms
   k <- length(p)   
   print(noquote(c("Number of design points", choose(N + k - 1, k - 1))))
   #  set for first patient
-  EN <- firstEN(p, early.stop,
+  EN <- firstEN(p, nmin, early.stop,
                 priora, priorb)  
   p_alloc <- c(1, Ea(buildp(EN)))
   #  expected allocation
@@ -21,13 +21,14 @@ pda <- function(N, p, early.stop=FALSE,
   #  for each additional patient
   for(i in 2 : N) {    
     #  tree set for next patient
-    EN <- nextEN(EN, p, early.stop,
+    EN <- nextEN(EN, p, nmin, early.stop,
                  priora, priorb) 
     #  build table of expected treatment arm allocation
     alloc <- buildp(EN)
     
     p_new <- rowSums(apply(alloc, 1, function(x){
-      x[length(p)+1]*ERN1(n=unlist(x[1:length(p)]), p, early.stop, priora, priorb)
+      x[length(p)+1]*ERN1(n=unlist(x[1:length(p)]), p, 
+                          nmin, early.stop, priora, priorb)
     }))
     p_alloc <- rbind(p_alloc, c(i,p_new))
     EaN <- rbind(EaN,c(i, Ea(alloc))) 
